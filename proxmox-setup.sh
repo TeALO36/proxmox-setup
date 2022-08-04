fa86f889e3326e96cdf59d6d16ffb1118b2a1bd4
TeALO36 <83779026+TeALO36@users.noreply.github.com>
2022-08-04 15:41:42 +0200

    Update proxmox-setup.sh

--- proxmox-setup.sh
+++ proxmox-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-bash <(curl -s https://raw.githubusercontent.com/Tontonjo/proxmox_toolbox/main/proxmox_toolbox.sh )
-
 bash <(curl -s https://raw.githubusercontent.com/Weilbyte/PVEDiscordDark/master/PVEDiscordDark.sh ) install
+
+bash <(curl -s https://raw.githubusercontent.com/Tontonjo/proxmox_toolbox/main/proxmox_toolbox.sh )
